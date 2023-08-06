e1eaee44c7dcfa908f553a78d48ac0ebcde4ba57
Tobias <tobiasrempe@zyonicsoftware.com>
2023-08-06 23:57:04 +0200

    Changes by Joshua

--- src/main/kotlin/util/gui/GUIEH.kt
+++ src/main/kotlin/util/gui/GUIEH.kt
@@ -5,7 +5,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
 
-class GUIEH : Listener {
+class GUIEH : Listener {  // Graphical User Interface Event Handler
     @EventHandler
     fun onInventoryClick(event: InventoryClickEvent) {
         if (event.inventory.holder !is IGUI) return
